0eadb6409650eef2b7c8bcba298a02bd718b9da8
Vadim Brusko <brva@ciklum.com>
2017-11-01 13:09:59 +0200

    Tracking screen UI

---
+++ RxLocation/Tracking/TrackingReactor.swift
@@ -0,0 +1,9 @@
+//
+//  TrackingReactor.swift
+//  RxLocation
+//
+//  Created by Vadym Brusko on 11/1/17.
+//  Copyright © 2017 Vadim Inc. All rights reserved.
+//
+
+import Foundation
---
+++ RxLocation/Tracking/TrackingViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TrackingViewController.swift
+//  RxLocation
+//
+//  Created by Vadym Brusko on 11/1/17.
+//  Copyright © 2017 Vadim Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TrackingViewController: UIViewController {
+  
+}
